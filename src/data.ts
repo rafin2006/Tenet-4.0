14433ccd89bd303a62344dfd6154264b28290b0f
Rafin Shah <rafinshah2006@gmail.com>
2026-04-08 17:57:05 +0600

    fresh start

---
+++ src/data.ts
@@ -0,0 +1,60 @@
+export const LINKS: Record<string, Record<number, string | string[]>> = {
+  p1:{2:'https://youtu.be/pc42PaR2OE8?list=PL19yQhk-X_pxtByoSG4zxN97pkjWLcuNt',3:'https://youtu.be/7OmrPXs_u2o',4:'https://youtu.be/w6zMqa9d2_0?list=PL19yQhk-X_pxtByoSG4zxN97pkjWLcuNt',5:'https://youtu.be/smEzjF7b-Sc?list=PL19yQhk-X_pxtByoSG4zxN97pkjWLcuNt',6:'https://youtu.be/GTbjmYNs9bs?list=PL19yQhk-X_pxtByoSG4zxN97pkjWLcuNt',7:'https://youtu.be/PNpEI3lQdb8?list=PL19yQhk-X_pxtByoSG4zxN97pkjWLcuNt',8:'https://youtu.be/K-OzRLh5700?list=PL19yQhk-X_pxtByoSG4zxN97pkjWLcuNt',9:'https://youtu.be/fzhyLLdr5PI',10:'https://youtu.be/oaisKu1kM2M?list=PL19yQhk-X_pxtByoSG4zxN97pkjWLcuNt'},
+  p2:{1:'https://youtu.be/vJMAqRV-5Co',2:'https://youtu.be/n0QhdCqiVEQ',3:'https://youtu.be/oDTeoyM9xDs',4:'https://youtu.be/svBkBL5Iv2U',5:'https://www.youtube.com/playlist?list=PLZ1LyDsSFxfRM2LFoLtbR43678hmkuSjK',6:'https://youtu.be/3ERrKtE4SVU',7:'https://youtu.be/3uA9dDlopsc',8:'https://youtu.be/ywGvOW6si3w',9:'https://youtu.be/SwWV4ql6l5w',10:'https://youtu.be/Kllvn-X5vLY',11:'https://youtu.be/X0tQEmcKYfw'},
+  c1:{1:'https://www.youtube.com/playlist?list=PLjv3eDZNO_oIzzEEKr_Cu1-BDokCPm0Pf',2:'https://youtu.be/Zz_fOmRFN88?list=PL1GVU-Q4Oos54lzADCT91q_qaAbBuPuVP',3:'https://youtu.be/DzWmJn8nVL4',4:'https://youtu.be/H83W_HTlzlc?list=PL1GVU-Q4Oos54lzADCT91q_qaAbBuPuVP',5:'https://youtu.be/jRT3nJSi2XA'},
+  c2:{1:'https://youtu.be/LZC1Hm4_Tr8?list=PLKjdwUViO6FiGS5H4TqGSbzcpu76V5KWU',2:'https://www.youtube.com/playlist?list=PL_LyKIs_ZVt2C6ccz4DTdvP84ao99QXA2',3:'https://youtu.be/rjoOajoAWWI?list=PLKjdwUViO6FiGS5H4TqGSbzcpu76V5KWU',4:'https://youtu.be/rL1896WVZNU?list=PLKjdwUViO6FiGS5H4TqGSbzcpu76V5KWU',5:'https://www.youtube.com/playlist?list=PLubWB9tWo5lV8aMtxPY5w4wJNKdX_xn3b'},
+  b1:{1:'https://youtu.be/bmrZbBM9uN8?list=PLoYieD8GCtx_IMSAx9saIg82UHxNH-pLm',2:'https://youtu.be/yLSjcbOt8qI?list=PLoYieD8GCtx_IMSAx9saIg82UHxNH-pLm',3:'https://youtu.be/GaTugkeRUmA?list=PLoYieD8GCtx_IMSAx9saIg82UHxNH-pLm',4:'https://youtu.be/r-Yf80ieVU4?list=PLoYieD8GCtx_IMSAx9saIg82UHxNH-pLm',5:'https://youtu.be/-jMAp6tSUl4',6:'https://youtu.be/PWagkOyd5EY',7:'https://youtu.be/e4mX4IFfVxg?list=PLoYieD8GCtx_IMSAx9saIg82UHxNH-pLm',8:'https://youtu.be/sYlWwN2eXls?list=PLoYieD8GCtx_IMSAx9saIg82UHxNH-pLm',9:'https://youtu.be/spKE10OOvUc?list=PLoYieD8GCtx_IMSAx9saIg82UHxNH-pLm',10:'https://youtu.be/ORyU7kHU8Bc?list=PLoYieD8GCtx_IMSAx9saIg82UHxNH-pLm',11:'https://youtu.be/VaOREawhUUw?list=PLoYieD8GCtx_IMSAx9saIg82UHxNH-pLm',12:'https://youtu.be/xhiC4W54lEU?list=PLg6DwUvmO07i2PcZvgJU7RO5acklq6435'},
+  b2:{1:'https://youtu.be/LrklqNYuQc4',2:'https://youtu.be/wjnPT32kocI',3:'https://youtu.be/CNA2Wui5NKE',4:'https://youtu.be/MiWb7xid8To?list=PLKoOxJAVOxyd1cUFoze6FCXqWJL2KWAEv',5:'https://youtu.be/VXR1lsheFQc?list=PLKoOxJAVOxyd1cUFoze6FCXqWJL2KWAEv',6:'https://youtu.be/tBDjL42Y1WU',7:'https://youtu.be/pqk8WJOGfC8?list=PLKoOxJAVOxyd1cUFoze6FCXqWJL2KWAEv',8:'https://youtu.be/atusvJ7_iMc?list=PLg6DwUvmO07i2PcZvgJU7RO5acklq6435',9:'https://youtu.be/6x-UuOLSlfk',10:'https://youtu.be/zkzpdxQHw9g?list=PLg6DwUvmO07i2PcZvgJU7RO5acklq6435',11:'https://youtu.be/K-dERoYfVOw?list=PLKoOxJAVOxyd1cUFoze6FCXqWJL2KWAEv'},
+  m1:{1:'https://youtu.be/KtqyZ-ANMUk?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih',2:'https://youtu.be/JLuKC_yxoWs?list=PLi5fEbOt0If7Rky9DdZePw4wNFJVbGxt4',3:'https://youtu.be/-WP3xRkTuaM?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih',4:'https://youtu.be/1jjTSSpq2pQ?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih',5:'https://youtu.be/pU_iwduItHs',6:'https://youtu.be/9pwpsJD0tL8?list=PLi5fEbOt0If7Rky9DdZePw4wNFJVbGxt4',7:'https://youtu.be/piZLlyz3wbs?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih',8:'https://youtu.be/oYeRl4OQo0Y',9:'https://youtu.be/DKSpBV109fE?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih',10:'https://youtu.be/WXMaW-VdpzA?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih'},
+  m2:{1:'https://youtu.be/6hhbuYeI1cU?list=PLi5fEbOt0If7Rky9DdZePw4wNFJVbGxt4',2:'https://youtu.be/C1OAPt1Q26U?list=PLi5fEbOt0If7Rky9DdZePw4wNFJVbGxt4',3:'https://youtu.be/yO-qpIO6rQU?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih',4:'https://youtu.be/KBrLO601OVU?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih',5:'https://youtu.be/g4dmJ7vVY5U?list=PLi5fEbOt0If7Rky9DdZePw4wNFJVbGxt4',6:'https://youtu.be/e5UjvcHh87E?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih',7:'https://youtu.be/dxU1TOy9jvM?list=PLA-A92FsFtb9WE12mqRL_7N8q2JMkY6Ih',8:'https://youtu.be/n9kLnm_Tqg8?list=PLi5fEbOt0If7Rky9DdZePw4wNFJVbGxt4',9:'https://youtu.be/K5ddkgvdVvM?list=PLi5fEbOt0If7Rky9DdZePw4wNFJVbGxt4',10:'https://youtu.be/9wdcmsJcHXI?list=PLi5fEbOt0If7Rky9DdZePw4wNFJVbGxt4'}
+};
+
+export const SUBJ: Record<string, {cls: string, key: string, short: string, color: string}> = {
+  'Physics 1st':  {cls:'p1',key:'p1',short:'Phy 1st', color: '#38bdf8'},
+  'Physics 2nd':  {cls:'p2',key:'p2',short:'Phy 2nd', color: '#60a5fa'},
+  'Chemistry 1st':{cls:'c1',key:'c1',short:'Chem 1st', color: '#34d399'},
+  'Chemistry 2nd':{cls:'c2',key:'c2',short:'Chem 2nd', color: '#10b981'},
+  'Biology 1st':  {cls:'b1',key:'b1',short:'Bio 1st', color: '#fbbf24'},
+  'Biology 2nd':  {cls:'b2',key:'b2',short:'Bio 2nd', color: '#fb923c'},
+  'Math 1st':     {cls:'m1',key:'m1',short:'Math 1st', color: '#a78bfa'},
+  'Math 2nd':     {cls:'m2',key:'m2',short:'Math 2nd', color: '#c084fc'},
+};
+
+export type ScheduleEntry = [string, number | number[]] | null;
+export type ScheduleRow = [number, string, string, ScheduleEntry, ScheduleEntry, ScheduleEntry, boolean?];
+
+export const SCHEDULE: ScheduleRow[] = [
+  [1, 'Apr 10','Fri',['Physics 1st',2],  ['Biology 1st',4],    ['Math 1st',1]],
+  [2, 'Apr 11','Sat',['Physics 1st',2],  ['Biology 1st',1],    ['Math 1st',2]],
+  [3, 'Apr 12','Sun',['Chemistry 1st',2],['Biology 1st',1],    ['Math 1st',3]],
+  [4, 'Apr 13','Mon',['Chemistry 1st',2],['Biology 1st',1],    ['Math 1st',3]],
+  [5, 'Apr 14','Tue',['Chemistry 1st',3],['Biology 1st',3],    ['Math 1st',3]],
+  [6, 'Apr 15','Wed',['Chemistry 1st',3],['Physics 1st',4],    ['Math 1st',4]],
+  [7, 'Apr 16','Thu',['Physics 1st',8],  ['Physics 1st',4],    ['Math 1st',5]],
+  [8, 'Apr 17','Fri',['Physics 1st',4],  ['Math 2nd',6],       null],
+  [9, 'Apr 18','Sat',['Chemistry 1st',3],['Biology 1st',3],    ['Math 1st',7]],
+  [10,'Apr 19','Sun',['Math 1st',3],     ['Biology 1st',3],    ['Math 1st',7]],
+  [11,'Apr 20','Mon',['Chemistry 1st',4],['Biology 1st',7],    ['Math 1st',8]],
+  [12,'Apr 21','Tue',['Chemistry 1st',4],['Biology 1st',8],    ['Math 1st',9]],
+  [13,'Apr 22','Wed',['Chemistry 1st',5],['Biology 1st',11],   ['Math 1st',9]],
+  [14,'Apr 23','Thu',['Physics 1st',10], ['Biology 2nd',2],    ['Math 1st',10]],
+  [15,'Apr 24','Fri',['Physics 1st',6],  ['Biology 2nd',2],    ['Math 1st',10]],
+  [16,'Apr 25','Sat',['Physics 2nd',1],  ['Biology 2nd',3],    ['Math 1st',10]],
+  [17,'Apr 26','Sun',['Chemistry 2nd',1],['Biology 2nd',[4,5]],['Math 1st',10]],
+  [18,'Apr 27','Mon',['Chemistry 2nd',2],['Biology 2nd',6],    ['Math 1st',10]],
+  [19,'Apr 28','Tue',['Chemistry 2nd',2],['Biology 2nd',7],    ['Math 2nd',3]],
+  [20,'Apr 29','Wed',['Chemistry 2nd',2],['Biology 2nd',8],    ['Math 2nd',4]],
+  [21,'Apr 30','Thu',['Chemistry 2nd',2],['Biology 2nd',8],    ['Math 2nd',4]],
+  [22,'May 01','Fri',['Physics 2nd',2],  ['Biology 1st',9],    ['Math 2nd',1]],
+  [23,'May 02','Sat',['Physics 2nd',3],  ['Biology 1st',9],    ['Math 2nd',7]],
+  [24,'May 03','Sun',['Physics 2nd',3],  ['Biology 1st',9],    ['Math 2nd',7]],
+  [25,'May 04','Mon',['Physics 2nd',4],  ['Biology 1st',11],   ['Math 2nd',6]],
+  [26,'May 05','Tue',['Physics 2nd',5],  ['Biology 1st',12],   ['Math 2nd',7]],
+  [27,'May 06','Wed',['Physics 2nd',6],  ['Biology 2nd',1],    ['Math 2nd',7]],
+  [28,'May 07','Thu',['Physics 2nd',7],  ['Biology 2nd',9],    ['Math 2nd',8]],
+  [29,'May 08','Fri',['Physics 2nd',8],  ['Biology 2nd',9],    ['Math 2nd',8]],
+  [30,'May 09','Sat',['Physics 2nd',9],  ['Biology 2nd',10],   ['Math 2nd',9]],
+  [31,'May 10','Sun',['Physics 2nd',10], ['Biology 2nd',11],   ['Math 2nd',9]],
+  [32,'May 11','Mon',['Physics 2nd',11], ['Biology 1st',2],    ['Math 2nd',10]],
+  [33,'May 12','Tue',['Physics 1st',3],  null,                 null,true],
+];
